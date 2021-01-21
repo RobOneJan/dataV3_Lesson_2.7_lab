ebb2b83559cfd334f29df11290e74174f89c70bb
RobOneJan <Robertm93040@gmail.com>
2021-01-21 18:57:39 +0100

    lab1_Day4_Week2 try 2

--- Lab1_day4_week2.sql
+++ Lab1_day4_week2.sql
@@ -41,3 +41,53 @@ UPDATE sakila.films_2020 SET rental_rate = 2.99;
 UPDATE sakila.films_2020 SET replacement_cost = 8.99;
 
 select * from sakila.films_2020;
+
+
+#In the table actor, which are the actors whose last names are not repeated? For example if you 
+#would sort the data in the table actor by last_name, you would see that there is Christian Arkoyd, Kirsten Arkoyd, and Debbie Arkoyd. These three actors have the same last name. So we do not want to include this last name in our output. Last name "Astaire" is present only one time with actor "Angelina Astaire", hence we would want this in our output list.
+
+
+SELECT last_name, count 
+from sakila.actor
+group by last_name;
+
+#Which last names appear more than once? We would use the same logic as in the previous question but this time we want to include the last names of the actors where the last name was present more than once
+
+select last_name, count(last_name)
+from actor
+group by last_name;
+
+
+#Using the rental table, find out how many rentals were processed by each employee.
+
+select staff_id, count(rental_id) as proccsed
+from rental
+group by staff_id;
+
+
+#Using the film table, find out how many films were released each year.
+Select release_year, count(film_id) as films
+from film
+group by release_year;
+
+
+#Using the film table, find out for each rating how many films were there.
+select  rating,count(film_id) as filmsperRating
+from film
+group by rating;
+
+
+
+#What is the mean length of the film for each rating type. Round off the average lengths to two decimal places
+
+select rating, round(avg(length),2)
+from film
+group by rating;
+
+
+#Which kind of movies (rating) have a mean duration of more than two hours?
+select rating, round(avg(length),2) as mittel
+from film
+group by rating
+having mittel > 120
+;
